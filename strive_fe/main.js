8e7d001efa7a4c00bf81017e80787e913036cfc4
GoesUp <gregor@gabrovsek.si>
2019-05-19 10:49:56 +0200

    Improved look of the application

--- strive_fe/main.js
+++ strive_fe/main.js
@@ -91,5 +91,51 @@ $(document).ready(function() {
     const avatarNejc = "<p class='image is-32x32'> <img src='http://www.iconninja.com/files/95/401/67/avatar-man-old-person-male-mature-user-icon.png'> </p>"
     const avatarZan = "<p class='image is-32x32'> <img src='http://www.iconninja.com/files/852/501/256/avatar-boy-young-user-kid-child-male-icon.png'> </p>"
     const avatarGregor= "<p class='image is-32x32'> <img src='http://www.iconninja.com/files/511/390/516/office-user-costume-business-avatar-man-male-icon.png'> </p>"
-    const feedTemplate = "<div class='box'> <article class='media is-primary'> <figure class='media-left'> ##_AVATAR_## </figure> <div class='media-content'> <div class='content'> <p> <strong>##_FIRST_NAME_## ##_LAST_NAME_##</strong> <small>##_EMAIL_##</small> <small>##_AGO_##</small> <br> ##_TITLE_## </p> </div> <nav class='level is-mobile'> <div class='level-left'> <a class='level-item'> <span class='icon is-small'><i class='fas fa-reply'></i></span> </a> <a class='level-item'> <span class='icon is-small'><i class='fas fa-hand-rock'></i></span>&nbsp ##_LIKES_## </a> </div> </nav> </div> </article> <article class='media'> <figure class='media-left'> <p class='image is-32x32'> <img src='images/portrait4.jpg'> </p> </figure> <div class='media-content'> <div class='field'> <p class='control'> <!-- <input class='input' type='text' placeholder='Input a comment...'> --> <textarea class='textarea' placeholder='Comment something...' rows='1'></textarea> </p> </div> <nav class='level'> <div class='level-left'> <div class='level-item'> </div> </div> <div class='level-right'> <div class='level-item'> <a class='button is-info is-small'>Submit</a> </div> </div> </nav> </div> </article> </div>"
+    const feedTemplate = `<div class='box'>
+  <article class='media is-primary'>
+    <figure class='media-left'> ##_AVATAR_## </figure>
+    <div class='media-content'>
+      <div class='content'>
+        <p> <strong>##_FIRST_NAME_## ##_LAST_NAME_##</strong> <small>##_EMAIL_##</small> <small>##_AGO_##</small> <br> ##_TITLE_## </p>
+      </div>
+      <nav class='level is-mobile'>
+        <div class='level-left'>
+        <a class='level-item'>
+        <div class="boutline" style="border:1px solid purple; margin:5px; width:50px; height:50px; border-radius:50px;">
+          <span class='icon is-small superbutton' style="margin:16px;">
+            <i class='fas fa-trophy' style="font-size: 175%"></i>
+          </span></div>
+          &nbsp ##_LIKES_##
+        </a>
+          <a class='level-item'>
+            <span class='icon is-small'>
+              <i class='fas fa-reply'></i>
+            </span>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </article>
+  <article class='media'>
+    <figure class='media-left'>
+      <p class='image is-32x32'> <img src='images/portrait4.jpg'> </p>
+    </figure>
+    <div class='media-content'>
+      <div class='field'>
+        <p class='control'>
+          <!-- <input class='input' type='text' placeholder='Input a comment...'> -->
+          <textarea class='textarea' placeholder='Comment something...' rows='1'></textarea>
+        </p>
+      </div>
+      <nav class='level'>
+        <div class='level-left'>
+          <div class='level-item'> </div>
+        </div>
+        <div class='level-right'>
+          <div class='level-item'> <a class='button is-info is-small'>Submit</a> </div>
+        </div>
+      </nav>
+    </div>
+  </article>
+</div>`
 });
